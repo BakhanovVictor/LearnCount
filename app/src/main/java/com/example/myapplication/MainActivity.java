2bddf227dcf07bae2649d42e90c505b79936e5aa
BakhanovVictor <bahanov@yandex.ru>
2023-11-07 21:27:58 +0800

    Поменял вопросы на математические примеры.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,54 +9,60 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     Button yesBtn;
     Button noBtn;
-    Questions[] questions = {
-            new Questions(R.string.question1, true),
-            new Questions(R.string.question2, true),
-            new Questions(R.string.question3, false),
-            new Questions(R.string.question4, false),
-            new Questions(R.string.question5, true)
-    };
-    boolean[] userAnswers = new boolean[5];
-    int questionIndex = 0;
+    int rndA;
+    int rndB;
+    int result;
+    int rightAnswers = 0;
+    int wrongAnswers = 0;
     TextView askTextView;
+    TextView rightAnswersTextView;
+    TextView wrongAnswersTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         yesBtn = findViewById(R.id.yesBtn);
         yesBtn.setOnClickListener((view) -> {
-            checkAnswer(questions[questionIndex].isAnswer());
+            checkAnswer(rndA, rndB, result, true);
         });
         noBtn = findViewById(R.id.noBtn);
         noBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkAnswer(!questions[questionIndex].isAnswer());
+                checkAnswer(rndA, rndB, result, false);
 
             }
         });
         askTextView = findViewById(R.id.askTextView);
-        askTextView.setText(questions[questionIndex].getText());
+        rightAnswersTextView = findViewById(R.id.rightAnswersTextView);
+        wrongAnswersTextView = findViewById(R.id.wrongAnswersTextView);
+        question();
     }
-    private void checkAnswer(boolean isAnswer){
-        userAnswers[questionIndex] = isAnswer;
+    private void checkAnswer(int valA, int valB, int result, boolean answer){
+        boolean isAnswer = !((valA+valB == result) ^ answer);
         if (isAnswer){
-            Toast.makeText(MainActivity.this, "Правильно!", Toast.LENGTH_SHORT).show();
+            // Toast.makeText(MainActivity.this, "Правильно!", Toast.LENGTH_SHORT).show();
+            rightAnswers++;
+            rightAnswersTextView.setText("Правильно - "+rightAnswers);
         } else {
-            Toast.makeText(MainActivity.this, "Неправильно!", Toast.LENGTH_SHORT).show();
+            // Toast.makeText(MainActivity.this, "Неправильно!", Toast.LENGTH_SHORT).show();
+            wrongAnswers++;
+            wrongAnswersTextView.setText("Неправильно - "+wrongAnswers);
         }
-        if((questionIndex+1) == questions.length) {
-            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-            intent.putExtra("questions", questions);
-            intent.putExtra("userAnswers", userAnswers);
-            startActivity(intent);
-            questionIndex = 0;
-        } else {
-            questionIndex++;
-        };
-        askTextView.setText(questions[questionIndex].getText());
+        question();
+    }
+    private void question() {
+        Random rnd = new Random();
+        rndA = rnd.nextInt(10)+1;
+        rnd = new Random();
+        rndB = rnd.nextInt(10)+1;
+        result = rnd.nextInt(3)+1;
+        result = rndA + rndB + result -1;
+        askTextView.setText(rndA+" + "+rndB+" = "+result);
     }
 }
